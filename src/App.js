72b7d2a86db041d820f8de31feec93a86d677643
João Costa <costav1402@gmail.com>
2021-10-09 12:35:04 -0300

    implemented connection with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import logo from './assets/logo.png'
 import Card from './components/Card/Card';
 function App() {
-  const [pokemon, setPokemon] =useState(null);
-  useEffect(()=>{
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [isFetch, setIsFetch] = useState(false);
+  async function getPokemon(){
+    setLoading(true);
     const randomId = Math.round(Math.random() * 890);
     const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-    axios.get(url).then(res=>{
-      console.log(res.data)
-      setPokemon(res.data)
-    })
-  },[])
-  if(!pokemon)return<div>Carregando...</div>
+    const response = await axios.get(url);
+    const data = response.data;
+    setIsFetch(true);
+    setLoading(false);
+    setPokemon(data);
+  }
   return (
     <div className="App">
       <div className="app__container">
-      <img src={logo} alt="logo" draggable="false"/>
-      <button type="submit">Click Para Sortear um Pokemon</button>
-      <Card pokemon={pokemon}/>
+      <img className="app__container-logo" src={logo} alt="logo" draggable="false"/>
+      <button className="app__container-btn" type="submit" onClick={getPokemon} disabled={loading}>Sorteie um Pokemon</button>
+      {isFetch &&(<Card pokemon={pokemon} loading={loading}/>)}
     </div>
       </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Card.css'
-export default function Card({pokemon}) {
+import not_found from '../../assets/not_found.png'
+export default function Card({pokemon, loading}) {
     return (
         <div className="card">
+        {loading &&( <div className="card__loading">Carregando...</div> )}
+        {pokemon && !loading &&(
+              <>
             <span className="card__glassphormism">{pokemon.name}</span>
             <div className="card__info">
-                <img className="card__info-image" src={pokemon.sprites.other['official-artwork']?.front_default} alt={pokemon.name}/>
+                <img className="card__info-image" src={(pokemon.sprites.other['official-artwork']?.front_default)?(pokemon.sprites.other['official-artwork']?.front_default):(not_found)} alt={pokemon.name}/>
         <h1 className="card__info-title" >{pokemon.name}</h1>
         <div className="card__info-types">
         {pokemon.types.map((type, id)=>(
@@ -13,6 +17,8 @@ export default function Card({pokemon}) {
         ))}
         </div>
         </div>
+        </>
+          ) }
       </div>
     )
 }
